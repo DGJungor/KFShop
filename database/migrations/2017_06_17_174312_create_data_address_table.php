62ff8ed34c05fa81932c36ae9ff1cfc5a840f0ae
liuzhiqi <CrossStarlight@163.com>
2017-06-20 13:48:05 +0800

    字段错误已修复

--- database/migrations/2017_06_17_174312_create_data_address_table.php
+++ database/migrations/2017_06_17_174312_create_data_address_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDataReceivingAddressTable extends Migration
+class CreateDataAddressTable extends Migration
 {
     /**
      * Run the migrations.
