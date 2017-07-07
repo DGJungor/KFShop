8ef9e761f92706c0e635b7fa8cfdff32afd5e8ff
pan <910421602@qq.com>
2017-07-07 11:28:42 +0800

    调整数据

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,8 +19,8 @@ class HomeController extends Controller
     {
         $friend=Friend::all();
         $banner=ShopBanner::paginate(4);
-        $banners=Recommend::paginate(4);
-        $res = compact("banner", "",["banners","friend"]);
+        $recommend=Recommend::paginate(4);
+//        $res = compact("banner", "",["banners","friend"]);
         $dataObj = \DB::table('data_types')->where('pid', '0')->get();
         foreach($dataObj as $data){
             $data->children = \DB::table('data_types')->where('pid', $data->id)->get();
@@ -31,9 +31,7 @@ class HomeController extends Controller
             }
 
         }
-
-
-        return view('web.index', compact('data', "", ['dataObj', 'dataObj', 'res']));
+        return view('web.index', compact('data', "", ['dataObj', 'dataObj', 'res', 'friend', 'banner', 'recommend']));
 
     }
 
