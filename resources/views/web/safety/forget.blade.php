2dbb2647c2a50c0075c31166f4e998b46f2cb4f7
liuzhiqi <CrossStarlight@163.com>
2017-07-10 02:51:50 +0800

    修复重置密码出现的问题

--- resources/views/web/safety/forget.blade.php
+++ resources/views/web/safety/forget.blade.php
@@ -10,9 +10,7 @@
 
     <link rel="stylesheet" type="text/css" href="{{ url('/web/css/style.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ url('/web/css/shopping-mall-index.css') }}" />
-    <script type="text/javascript" src="{{ url('/web/js/jquery.js') }}"></script>
-    <script type="text/javascript" src="{{ url('/web/js/zhonglin.js') }}"></script>
-    <script type="text/javascript" src="{{ url('/web/js/zhongling2.js') }}"></script>
+    <script type="text/javascript" src="{{ url('/web/js/jquery.min.js') }}"></script>
 
 </head>
 
@@ -28,9 +26,9 @@
     <div class="psw">
         <p class="psw-p1">用户名</p>
         <input id="username" name="username" type="text" placeholder="用户名" />
-        <span id="name_dui" class="dui" style="display: none"></span>
-        <span id="name_cuo" class="cuo" style="display: none">用户名不存在</span>
-        <span id="name_must" class="cuo" style="display: none">请输入用户名</span>
+        <span id="name_dui" class="dui" style="display: none;"></span>
+        <span id="name_cuo" class="cuo" style="display: none;">用户不存在</span>
+        <span id="name_must" class="cuo" style="display: none;">请输入用户名</span>
     </div>
     <div class="psw psw2">
         <p class="psw-p1">邮箱</p>
@@ -43,18 +41,20 @@
     <div class="psw">
         <p class="psw-p1">验证码</p>
         <input id="code" name="code" type="text" placeholder="请输入邮箱验证码" />
+        <span id="code_cuo" class="cuo" style="display: none;">请输入邮箱码</span>
     </div>
     <div class="psw">
         <p class="psw-p1">输入密码</p>
         <input id="password" name="password" type="password" placeholder="请输入密码" />
-        <span id="pwd_tip">密码由6-16的字母、数字、符号组成</span>
-        <span id="pwd_easy" class="cuo">密码过于简单</span>
-        <span id="pwd_length" class="cuo">密码由6-60位的字母、数字、符号组成</span>
+        <span id="pwd_tip" style="display: none;">密码由6-16的字母、数字、符号组成</span>
+        <span id="pwd_easy" class="cuo" style="display: none;">密码过于简单</span>
+        <span id="pwd_length" class="cuo" style="display: none;">密码由6-60位的字母、数字、符号组成</span>
     </div>
     <div class="psw">
         <p class="psw-p1">确认密码</p>
         <input id="password_confirm" name="password" type="password" placeholder="请再次确认密码" />
-        <span class="cuo">密码不一致，请重新输入</span>
+        <span id="pwd_again" class="cuo" style="display: none;">请再次输入密码</span>
+        <span id="pwd_vary" class="cuo" style="display: none">密码不一致，请重新输入</span>
     </div>
     <button id="resetPwd" class="psw-btn">重置密码</button>
 </div>
@@ -83,6 +83,11 @@
         $('#name_cuo').hide();
         $('#name_must').hide();
         var username = $('#username').val();
+        if (username == '') {
+            $('#name_dui').hide();
+            $('#name_cuo').hide();
+            $('#name_must').show();
+        }
         $.ajax({
             type: 'POST',
             url: '/ajax/user/checkName',
@@ -94,12 +99,12 @@
                     return;
                 }
                 if (data.status == 3) {
-                    $('#name_dui').show();
                     $('#name_cuo').hide();
                     $('#name_must').hide();
+                    $('#name_dui').show();
                     return;
                 }
-                if (data.status = 0) {
+                if (data.status == 0) {
                     $('#name_dui').hide();
                     $('#name_must').hide();
                     $('#name_cuo').show();
@@ -119,6 +124,7 @@
         }
     });
 
+    //发送邮箱验证码
     $('#getCode').off().click(function () {
         if (!enable) {
             return false;
@@ -167,6 +173,12 @@
                     }, 1000);
                     return;
                 }
+                if (data.status == 2) {
+                    $('#email_cuo').hide();
+                    $('#email_dui').hide();
+                    $('#email_error').show();
+                    return;
+                }
             },
             error: function () {
                 $('#email_cuo').hide();
@@ -177,7 +189,12 @@
         });
     });
 
-    $('#resetPwd').off().click(function () {
+    //重置密码
+    $('#resetPwd').click(function () {
+        var username = $('#username').val();
+        var code = $('#code').val();
+        var password = $('#password').val();
+        var password_confirm = $('#password_confirm').val();
         if ($('#username').val() == '') {
             $('#name_dui').hide();
             $('#name_cuo').hide();
@@ -189,45 +206,66 @@
             $('#email_dui').hide();
             return false;
         }
-        if ($('#code').val() == '') {
+        if ( code== '') {
             $('#code_cuo').show();
             $('#code_dui').hide();
             return false;
         }
-        $password = $('#password').val();
-        if ($password == '') {
-            $('#password_cuo').show();
-            $('#password_dui').hide();
+        if (password == '') {
+            $('#pwd_tip').show();
+            $('#pwd_easy').hide();
+            $('#pwd_length').hide();
             return false;
         }
-        if (!preg_match("/^(?![0-9]+$)(?![a-zA-Z]+$)(?!([^(0-9a-zA-Z)]|[\(\)])+$)([^(0-9a-zA-Z)]|[\(\)]|[a-zA-Z]|[0-9]){6,60}$/",$password)) {
-
+        var checkPwd = /^(?![0-9]+$)(?![a-zA-Z]+$)(?!([^(0-9a-zA-Z)]|[\(\)])+$)([^(0-9a-zA-Z)]|[\(\)]|[a-zA-Z]|[0-9]){6,60}$/;
+        if (!checkPwd.test(password)) {
+            $('#pwd_easy').show();
+            $('#pwd_tip').hide();
+            $('#pwd_length').hide();
         }
-        $password_confirm = $('#password_confirm').val();
-        if ($password_confirm== '') {
-            $('#password_confirm').show();
+        if (password.length > 60 || password.length < 6){
+            $('#pwd_length').show();
+            $('#pwd_easy').hide();
+            $('#pwd_tip').hide();
+        }
+        if (password_confirm== '') {
+            $('#pwd_vary').hide();
+            $('#pwd_again').show();
+            return false;
+        }
+        if (password != password_confirm) {
+            $('#pwd_again').hide();
+            $('#pwd_vary').show();
             return false;
         }
 
-
-        var username = $('#username').val();
         $.ajax({
             type: 'POST',
             url: '/forget/resetPassword',
             dataType: 'json',
-            data: {username: username, email: email, _token: "{{csrf_token()}}"},
+            data: {username: username,code: code, password: password,password_confirm: password_confirm, _token: "{{csrf_token()}}"},
             success: function (data) {
                 if (data == null) {
                     layer.msg('服务器端错误', 1, 0);
                     return;
                 }
                 if (data.status == 0) {
+                    $('#username').val('');
+                    $('#email').val('');
+                    $('#code').val('');
+                    $('#password').val('');
+                    $('#password_confirm').val('');
+                    layer.msg('重置密码成功',1,1);
                     return;
                 }
-                if (data.status == 1) {
-                    layer.msg('数据异常', 1, 0);
+                if (data.status != 0) {
+                    layer.msg(data.message, 1, 8);
                     return;
                 }
+            },
+            error: function () {
+                layer.msg('服务器异常',1,0);
+                return;
             }
         });
     });
